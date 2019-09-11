4fe9859a6acb56d099b9d5fcd495c31ad031d4cd
alexandre-mace <contact.alexandremace@gmail.com>
2019-09-11 13:54:42 +0200

    [API] Add task entity and relative repository

---
+++ api/src/Entity/Task.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $hardness;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default" : 0})
+     */
+    private $done = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getHardness(): ?int
+    {
+        return $this->hardness;
+    }
+
+    public function setHardness(int $hardness): self
+    {
+        $this->hardness = $hardness;
+
+        return $this;
+    }
+
+    public function getDone(): ?bool
+    {
+        return $this->done;
+    }
+
+    public function setDone(bool $done): self
+    {
+        $this->done = $done;
+
+        return $this;
+    }
+}
